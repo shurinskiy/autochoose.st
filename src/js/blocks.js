59b14f5f7512eb558bb4c20318dbe39152fe26fb
Alexandr Tumanov <shurinskiy2@gmail.com>
2024-11-14 18:03:52 +0500

    add page done

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -6,3 +6,5 @@ import "../blocks/catalog/catalog.js";
 import "../blocks/d-gallery/d-gallery.js";
 import "../blocks/gallery/gallery.js";
 import "../blocks/faq/faq.js";
+import "../blocks/select/select.js";
+import "../blocks/pictures/pictures.js";
